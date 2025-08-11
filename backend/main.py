f04b6084346b8859b1f1fd169328266b339dcec8
Raquel Paradella <raquelparadella@gmail.com>
2025-08-11 10:20:18 -0300

    Configuração do ambiente

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,18 @@
+# cria aplicação FastAPI
+# define uma única rota (endpoint) na raiz (/)
+
 from fastapi import FastAPI
 
-app = FastAPI()
+# Cria a instância principal da aplicação FastAPI
+app = FastAPI(
+    title="Amora Hub API",
+    description="API para o gerenciamento colaborativo de atividades domésticas.",
+    version="0.1.0"
+)
 
 @app.get("/")
-def root():
-    return {"message": "Amora Hub API funcionando"}
+def read_root():
+    """
+    Endpoint raiz que retorna uma mensagem de boas-vindas.
+    """
+    return {"Project": "Bem-vindo(a) ao Amora Hub!"}
